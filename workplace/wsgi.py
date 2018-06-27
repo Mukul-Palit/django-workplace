7b43ac30499b0a213db99b84d592be0a74b7ccd5
Mukul-Palit <mukul.palit1997@gmail.com>
2018-06-27 14:09:44 +0530

    WSGI update

--- workplace/wsgi.py
+++ workplace/wsgi.py
@@ -13,10 +13,10 @@ import sys
 
 from django.core.wsgi import get_wsgi_application
 
-path = '/home/mukulpalit/workplace'
+path = '/home/mukulpalit/django-workplace'
 if path not in sys.path:
     sys.path.append(path)
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "workplace.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django-workplace.settings")
 
 application = get_wsgi_application()
