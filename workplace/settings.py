0ae12d474b95277475f7f8476c0f59621e854cba
Mukul-Palit <mukul.palit1997@gmail.com>
2018-06-27 13:45:33 +0530

    Settings update

--- workplace/settings.py
+++ workplace/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'v!+)hd8in%0(@o+t!rbl4$p584v-mr=+o(hqz%+0y&^dr33g(='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
